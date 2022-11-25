ac566533cbea259916b1d68cd1097b2133a8b413
Kirill Scherba <kirill@scherba.ru>
2022-11-26 00:22:34 +0300

    Page template updated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <button type="button" @click="getFortune()">Get a new fortune message</button>
+    <h1>Teonet Fortune single-page application</h1>
+    <p class="subj">
+        This simple single-page application calls Teonet fortune Google 
+        Cloud Function. Click the 'Click' button to execute 
+        fortune Google Cloud Function: 
+        <a href="https://fortune-2.teonet.app">https://fortune-2.teonet.app</a>
+    </p>
+    <button type="button" @click="getFortune()">Click</button>
     <div v-if="fortune != null">
       <p>{{ fortune }}</p>
     </div>
@@ -8,7 +15,6 @@
 </template>
 
 <script>
-
 export default {
   name: "App",
   components: {},
@@ -24,3 +30,18 @@ export default {
   },
 };
 </script>
+
+<style>
+    .container {
+        margin-top: 25px;
+        margin-bottom: 25px;
+        max-width: 835px;
+    }
+    .subj {
+        color: gray;
+        font-size: small;
+    }
+    .subj a {
+        color: gray;
+    }
+</style>
